3060dc5bce7b56ff344ba4e56e3dd0ef1cc546cf
hardik <kantariyahardik618@ggamil.com>
2025-01-17 18:56:33 +0530

    17/1/25

--- JAVASCRIPT/JSON/script.js
+++ JAVASCRIPT/JSON/script.js
@@ -14,12 +14,69 @@
 
 // json to javascript
 
-{
-    let obj = '{"name":"srushti" , "age" : 19}'
+// {
+//     let obj = '{"name":"srushti" , "age" : 19}'
 
-    console.log(obj);
+//     console.log(obj);
     
-    let jsobj = JSON.parse(obj)
-    console.log(jsobj);
+//     let jsobj = JSON.parse(obj)
+//     console.log(jsobj);
     
+// }
+
+// javascript fetch
+
+{
+    async function data(){
+        let res = await fetch("http://localhost:3000/profile")
+        let dataa = await res.json()
+        console.log(dataa.name);  
+    }
+
+    data()
 }
+
+//javascript function
+
+{
+    function sum(a,b){
+        console.log(a+b)
+    }
+
+    sum(10,20)
+}
+{
+    function sub([a,b]){
+        console.log(a-b);
+        
+    }
+    sub([10,20])
+}
+
+// javascript arrow function
+
+{
+    let func = (a,b) => console.log(a+b);
+    func(10,30)
+
+}
+
+const sum = () => {
+    console.log('helloooooooooooooooooooooooooooooo');
+}
+sum()
+
+// syncronous or asyncronous function
+
+/*
+setTimeout
+clearTimeout
+setInterval
+clearInterval
+*/
+
+setTimeout(() => {
+    console.log("heloooo guyessss");
+},10000)
+
+
