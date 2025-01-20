efdd6e4108763e25908559f82a66683897c4b5d2
Sahil Narola <sahilnarola123@gmail.com>
2025-01-20 12:12:05 +0530

    20/1

--- JAVASCRIPT/programs/script.js
+++ JAVASCRIPT/programs/script.js
@@ -41,8 +41,129 @@
 // JavaScript Program to Swap Two Variables
 
 {
+    let a = 10
+    let b = 20
+
+    a = a+b
+    b = a-b
+    a = a-b
+
+    console.log(`after swipe a=${a} and b=${b}`);
     
 }
+
+// 6) JavaScript Program to Solve Quadratic Equation
+
+// x=\frac{-b\pm \sqrt{b^2-4ac}}{2a}          
+
+{
+    function qe(a,b,c){
+        let d = b * b - 4 * a * c
+        let sqr = Math.sqrt(Math.abs(d))
+
+        if(d>0){
+            console.log(`roots are different`);
+            console.log(`${(-b + sqr) / (2 * a)} and ${(-b - sqr) / (2 * a)}`);
+        }
+        else if(d==0){
+            console.log(`roots are same`);
+            console.log(-b / (2 * a) `and` -b / (2 * a));
+        }else { 
+            console.log('Roots are complex'); 
+        }
+    }
+    qe(1,-3,2)
+}
+// 7) JavaScript Program to Convert Kilometres to Miles
+
+{
+    let mile = 0.621371
+    let km = 2
+
+    let convert = km * mile
+
+    console.log(`2 km is = ${convert} miles`);
+    
+}
+// 8) Javascript Program to Convert Celsius to Fahrenheit
+
+{
+    let c = 2
+    let f = (c * 9/5) + 32
+
+    let convert = c * f 
+
+    console.log(`2 celcious is = ${convert} fehrenhit`);
+    
+}
+// 9) Javascript Program to Generate a Random Number
+
+{
+    console.log(Math.random());
+    
+}
+
+// 10)Javascript Program to Check if a number is Positive, Negative, or Zero
+
+{
+    function check(a){
+        if(a==0 && b==0){
+            console.log(`${a} is zero`);
+        }
+        else if(a>0){
+            console.log(`${a} is positive number`);
+        }
+        else{
+            console.log(`${a} is nagetive number`);
+        }
+    }
+    check(10)
+}
+// 11) Javascript Program to Check if a Number is Odd or Even
+
+{
+    function check(a){
+        if(a % 2 == 0){
+            console.log(`${a} is even number`);
+        }else{
+            console.log(`${a} is an odd number`);           
+        }
+    }
+    check(7)
+}
+// 12)JavaScript Program to Find the Largest Among Three Numbers
+
+{
+    function largest(a,b,c){
+        if(a>b && a>c){
+            console.log(`${a} is largest`);
+        }
+        else if(b>a && b>c){
+            console.log(`${b} is largest`);
+        }
+        else{
+            console.log(`${c} is largest`);
+        }
+    }
+    largest(10,2000,100)
+}
+// 13)JavaScript Program to Check Prime Number
+
+{
+    function Prime(num) {
+        for (let i = 2; i < num; i++) {
+            if (num % i === 0 || num == 1 || num == 0) {
+                console.log(`${num} is not a prime number`);
+            }
+            else{
+                console.log(`${num} is a prime number`);               
+            }
+        }
+        
+    }
+    Prime(2)
+}
+// 14)JavaScript Program to Print All Prime Numbers in an Interval
 
 
 
