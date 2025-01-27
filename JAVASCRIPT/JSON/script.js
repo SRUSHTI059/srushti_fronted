499a52f92c0e90633e2249a2c53ae08d34cf7a90
Sahil Narola <sahilnarola123@gmail.com>
2025-01-27 11:53:56 +0530

    27

--- JAVASCRIPT/JSON/script.js
+++ JAVASCRIPT/JSON/script.js
@@ -201,3 +201,24 @@ clearInterval
 
 // DOM document object model
 
+function styles(){
+    let dom = document.querySelector('div')
+    dom.setAttribute('id' , 'firstDiv')
+    document.getElementById('firstDiv').style.backgroundColor = "red"
+}
+
+/* Document.getElementById() */
+
+{
+    let dom = document.getElementById('firstDiv').innerHTML
+
+    console.log(dom);
+
+    let dom1 = document.getElementById('firstDiv').textContent
+
+    console.log(dom1);
+
+    let  dom2 = document.getElementById('firstDiv').innerText
+
+    console.log(dom2);
+}
