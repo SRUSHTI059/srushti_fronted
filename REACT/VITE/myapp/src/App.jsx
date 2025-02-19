9610fc566783a6c5cf590dd7e02bf161f40452e2
Sahil Narola <sahilnarola123@gmail.com>
2025-02-19 11:33:40 +0530

    19/2

--- REACT/VITE/myapp/src/App.jsx
+++ REACT/VITE/myapp/src/App.jsx
@@ -1,27 +1,48 @@
-
+// import './index.css'
 import './App.css'
+import FirstComponents from './coponent/component'
+import { Image, Profile } from './coponent/profile'
 
-function App() {
-  
+// function App() {
 
+
+//   return (
+//     <>
+//       <h1 className='text-4xl text-red-500'>this is heading tag</h1>
+//         <div className='w-100 h-100 bg-purple-700 border mt-5 ml-[400px]'>
+//           <div className='h-90 w-90 bg-purple-600 border m-5'>
+//             <div className='h-80 w-80 bg-purple-500 border m-5'>
+//               <div className='h-70 w-70 bg-purple-400 border m-5'>
+//                 <div className='h-60 w-60 bg-purple-300 border m-5'>
+//                   <div className='h-50 w-50 bg-purple-200 border m-5'>
+//                     <h1 className='text-4xl text-purple-800 text-center mt-[70px]'>box</h1>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//     </>
+//   )
+// }
+
+// export default App
+
+// export default function Congratulations() {
+//   return (
+//     <h1>Good job!</h1>
+//   );
+// }
+
+function Application(){
   return (
     <>
-    <h1 className='text-4xl text-red-500'>this is heading tag</h1>
-    <div className='w-100 h-100 bg-purple-700 border mt-5 ml-[400px]'>
-      <div className='h-90 w-90 bg-purple-600 border m-5'>
-        <div className='h-80 w-80 bg-purple-500 border m-5'>
-          <div className='h-70 w-70 bg-purple-400 border m-5'>
-            <div className='h-60 w-60 bg-purple-300 border m-5'>
-              <div className='h-50 w-50 bg-purple-200 border m-5'>
-                <h1 className='text-4xl text-purple-800 text-center mt-[70px]'>box</h1>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <h1>heading tag</h1>
+    <FirstComponents/>
+    <Profile/>
+    <Image/>
     </>
   )
 }
 
-export default App
+export default Application
