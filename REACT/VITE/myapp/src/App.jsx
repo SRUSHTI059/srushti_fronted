d34eed53f22ee13f0f91b3ebb3804f42c0edce81
PayalPatil8980 <sq.payal30@gmail.com>
2025-03-04 18:50:15 +0530

    4/3/25

--- REACT/VITE/myapp/src/App.jsx
+++ REACT/VITE/myapp/src/App.jsx
@@ -1,32 +1,42 @@
 import './index.css'
 import './App.css'
+import Props from './reactprops/props'
+import Jsxmodule from '../jsxmodule/jsxmodule'
+import Demo from '../LISTMAP/Demo'
+import Image from '../LISTMAP/Image'
 // import FirstComponents from './coponent/component'
 // import { Image, Profile } from './coponent/profile'
 
-// function App() {
+function App() {
 
+  return (
+    // <>
+    //   <h1 className='text-4xl text-red-500'>this is heading tag</h1>
+    //     <div className='w-100 h-100 bg-purple-700 border mt-5 ml-[400px]'>
+    //       <div className='h-90 w-90 bg-purple-600 border m-5'>
+    //         <div className='h-80 w-80 bg-purple-500 border m-5'>
+    //           <div className='h-70 w-70 bg-purple-400 border m-5'>
+    //             <div className='h-60 w-60 bg-purple-300 border m-5'>
+    //               <div className='h-50 w-50 bg-purple-200 border m-5'>
+    //                 <h1 className='text-4xl text-purple-800 text-center mt-[70px]'>box</h1>
+    //               </div>
+    //             </div>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    // </>
 
-//   return (
-//     <>
-//       <h1 className='text-4xl text-red-500'>this is heading tag</h1>
-//         <div className='w-100 h-100 bg-purple-700 border mt-5 ml-[400px]'>
-//           <div className='h-90 w-90 bg-purple-600 border m-5'>
-//             <div className='h-80 w-80 bg-purple-500 border m-5'>
-//               <div className='h-70 w-70 bg-purple-400 border m-5'>
-//                 <div className='h-60 w-60 bg-purple-300 border m-5'>
-//                   <div className='h-50 w-50 bg-purple-200 border m-5'>
-//                     <h1 className='text-4xl text-purple-800 text-center mt-[70px]'>box</h1>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//     </>
-//   )
-// }
+    <div>
+        <Props/>
+        <Jsxmodule/>
+        <Demo/>
+        <Image/>
+    </div>
+  )
+}
 
-// export default App
+export default App
 
 // export default function Congratulations() {
 //   return (
